f6fb89d2df56d324c88463ff02d8db39eee65759
Riya Chhikara <115228191+RiyaChhikara@users.noreply.github.com>
2023-06-27 13:27:11 +0530

    Update Day 32.py

--- Day 32.py
+++ Day 32.py
@@ -43,3 +43,26 @@ name,email,year,month,day
 test,emailtest,2023,03,23
 '''
 
+
+"""
+SIMPLE MAIL TRANSFER PROTOCOL (SMTP)
+how to send email using the smtplib module and Python.
+1. Make sure you've got the correct smtp address for your email provider:
+
+Gmail: smtp.gmail.com
+Hotmail: smtp.live.com
+Outlook: outlook.office365.com
+Yahoo: smtp.mail.yahoo.com
+If you use another email provider, just Google for your email provider
+e.g. "Gmail SMTP address"
+Below are steps specific to users sending email from Gmail addresses.
+
+2. Make sure you've enabled less secure apps if you are sending from a
+Gmail account. (Refer to the video in the next lesson for steps).
+
+3. Try to go through the Gmail Captcha here while logged in to the Gmail
+account: https://accounts.google.com/DisplayUnlockCaptcha
+
+4. Add a port number by changing your code to this:
+smtplib.SMTP("smtp.gmail.com", port=587)
+"""
